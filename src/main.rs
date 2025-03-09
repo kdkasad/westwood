f1e04c8ac17d3fc9681d8f2585b7178562f3f8fc
Kian Kasad <kian@kasad.com>
2025-03-09 11:33:35 -0400

    Simplify checking for syntax errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::process::ExitCode;
 
 use crate::rules::api::Rule;
-use tree_sitter::{Language, Parser, Query, QueryCaptures, QueryCursor, StreamingIterator, Tree};
+use tree_sitter::{Parser, Tree};
 
 pub mod rules;
 pub mod helpers;
@@ -10,26 +10,6 @@ fn usage() {
     let maybe_argv0: Option<String> = std::env::args().nth(0);
     let executable: &str = maybe_argv0.as_deref().unwrap_or("westwood");
     eprintln!("Usage: {} <file>", executable);
-}
-
-fn find_errors(tree: &Tree, code: &[u8]) -> (u64, u64) {
-    let lang: Language = Language::from(tree_sitter_c::LANGUAGE);
-    let q_err_src = "(ERROR) @error";
-    let q_err = Query::new(&lang, q_err_src).expect("Failed to create query");
-    let mut cursor = QueryCursor::new();
-    let mut captures: QueryCaptures<_, _> = cursor.captures(&q_err, tree.root_node(), code);
-    let mut n_errors: u64 = 0;
-    while let Some(_capture) = captures.next() {
-        n_errors += 1;
-    }
-    let q_missing_src = "(MISSING) @missing";
-    let q_missing = Query::new(&lang, &q_missing_src).expect("Failed to create query");
-    captures = cursor.captures(&q_missing, tree.root_node(), code);
-    let mut n_missing = 0;
-    while let Some(_capture) = captures.next() {
-        n_missing += 1;
-    }
-    return (n_errors, n_missing);
 }
 
 fn main() -> ExitCode {
@@ -68,10 +48,10 @@ fn main() -> ExitCode {
     let tree: Tree = parser.parse(&code, None).expect("Failed to parse code");
 
     // Check for syntax errors
-    let (n_errors, n_missing) = find_errors(&tree, &code);
-    if n_errors > 0 || n_missing > 0 {
-        eprintln!("Found {} syntax errors.", n_errors);
+    if tree.root_node().has_error() {
+        eprintln!("Found syntax error(s) in your code.");
         eprintln!("Ensure your code compiles before running the linter.");
+        eprintln!("To prevent false positives, the linter will not check code with syntax errors.");
         return ExitCode::FAILURE;
     }
 
