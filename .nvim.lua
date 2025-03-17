b5b88bc5f49ba67515814577a0775d7516b90783
Kian Kasad <kian@kasad.com>
2025-03-17 14:35:46 -0700

    Add Neovim keybinding to make looking up grammar.js rules easier

--- .nvim.lua
+++ .nvim.lua
@@ -1,7 +1,22 @@
+-- Load custom injection queries
 local cwd = vim.fn.getcwd()
 if cwd:find("westwood$") then
     -- Include our injection queries
-	vim.opt.runtimepath:prepend(cwd .. "/.nvim")
+    vim.opt.runtimepath:prepend(cwd .. "/.nvim")
     -- Disable LSP string highlighting because it doesn't support nested injections
     vim.api.nvim_set_hl(0, "@lsp.type.string", {})
 end
+
+-- Make gd jump to rule definitions in grammar.js
+vim.api.nvim_create_autocmd({ "BufRead", "BufNew" }, {
+    pattern = "grammar.js",
+    group = vim.api.nvim_create_augroup("westwood", {}),
+    callback = function()
+        vim.api.nvim_buf_set_keymap(0, "n", "gd", "", {
+            callback = function()
+                -- Search for the word under the cursor followed by a colon
+                vim.fn.search("\\<" .. vim.fn.expand("<cword>") .. "\\>:", "scw")
+            end,
+        })
+    end,
+})
