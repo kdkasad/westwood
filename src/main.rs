c42f0ceb7cc927cc2051a2f8ddff55c76d5c7f89
Kian Kasad <kian@kasad.com>
2025-03-25 20:09:04 -0400

    Enable clippy::pedantic warnings

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![warn(clippy::pedantic)]
+
 use std::io::stdout;
 use std::io::IsTerminal;
 use std::process::ExitCode;
